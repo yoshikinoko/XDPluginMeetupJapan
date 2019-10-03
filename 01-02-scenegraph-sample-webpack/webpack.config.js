16368dba279ab547fa685bb31d196deeefa07da7
Yoshiki Takeoka <yoshikinoko@gmail.com>
2019-10-03 20:15:46 +0900

    console update v2

--- 01-02-scenegraph-sample-webpack/webpack.config.js
+++ 01-02-scenegraph-sample-webpack/webpack.config.js
@@ -1,23 +1,55 @@
+const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: __dirname,
-        filename: 'build/main.js',
-        libraryTarget: 'commonjs2',
-    },
-    devtool: 'none', // prevent webpack from using eval() on my module
-    externals: {
-        uxp: 'uxp',
-        scenegraph: 'scenegraph',
-        commands: 'commands',
-        clipboard: 'clipboard',
-    },
-    plugins: [new CopyWebpackPlugin([
-        { from: 'static', to: 'build/', toType: 'dir' },
-    ], { debug: 'debug' })],
-    module: {
-        rules: [],
-    },
+  entry: path.resolve(__dirname, 'src/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+    libraryTarget: 'commonjs2',
+  },
+  devtool: 'none', // prevent webpack from using eval() on my module
+  externals: {
+    application: 'application',
+    assets: 'assets',
+    clipboard: 'clipboard',
+    cloud: 'cloud',
+    commands: 'commands',
+    interactions: 'interactions',
+    scenegraph: 'scenegraph',
+    uxp: 'uxp',
+    viewport: 'viewport',
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: 'assets', to: './', toType: 'dir' }], {
+      debug: 'debug',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: 'commonjs',
+                targets: {
+                  node: 'current',
+                },
+              },
+            ],
+          ],
+          plugins: [['@babel/plugin-transform-modules-commonjs']],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
